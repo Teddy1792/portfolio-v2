1f73d1fead6c43498f3a94e59b76e6c50df313ae
Theodore <theodore.abitbol@gmail.com>
2024-07-18 16:32:56 +0200

    modified footer link

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,7 +3,7 @@ export default function NavBar() {
     <nav className="flex flex-row justify-between w-[320px] lg:w-full">
       <h1 className="text-3xl lg:text-5xl">THEO_AB.</h1>
       <div className="flex flex-row gap-4 items-center">
-        <a className="" href="mailto:theodore@gmail.com">
+        <a className="" target="blank" href="mailto:theodore@gmail.com">
           <img src="/email.svg"></img>
         </a>
         <a className="" href="https://www.linkedin.com/in/theodoreabitbol/">
