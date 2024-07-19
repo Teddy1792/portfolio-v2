4416e4e32a981a63e2a4b5fbda23a83130e77b9f
Theodore <theodore.abitbol@gmail.com>
2024-07-19 11:26:39 +0200

    added margin footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   var currentYear = new Date().getFullYear();
   return (
-    <footer className="flex flex-row gap-16 w-full justify-center p-[50px]">
+    <footer className="flex flex-row gap-16 w-full justify-center p-[50px] mt-20">
       <p>Year {currentYear}</p>
       <p> portfolio v2.0</p>
       <a href="https://portfolio-three-silk-95.vercel.app/" target="blank">
