13eab1c9904e690e7b0b62c4a603703fca74dbf0
Theodore <theodore.abitbol@gmail.com>
2024-07-22 10:23:40 +0200

    added github link and img

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,13 +1,27 @@
+import Image from "next/image";
+
 export default function NavBar() {
   return (
     <nav className="flex flex-row justify-between w-[320px] lg:w-full">
       <h1 className="text-3xl lg:text-5xl">THEO_AB.</h1>
       <div className="flex flex-row gap-4 items-center">
-        <a className="" target="blank" href="mailto:theodore@gmail.com">
-          <img src="/email.svg"></img>
+        <a className="" target="_blank" href="mailto:theodore@gmail.com">
+          <Image src="/email.svg" alt="Email" width={24} height={24} />
         </a>
-        <a className="" href="https://www.linkedin.com/in/theodoreabitbol/">
-          <img src="/linkedin.svg"></img>
+        <a
+          className=""
+          target="_blank"
+          href="https://www.linkedin.com/in/theodoreabitbol/"
+        >
+          <Image src="/linkedin.svg" alt="LinkedIn" width={24} height={24} />
+        </a>
+        <a className="" target="_blank" href="https://github.com/Teddy1792">
+          <Image
+            src="/github-original.svg"
+            alt="GitHub"
+            width={24}
+            height={24}
+          />
         </a>
       </div>
     </nav>
